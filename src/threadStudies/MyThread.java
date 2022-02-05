a0cd378571ad6b985eaf09181a1ce3af9e0da2f5
Antonio Amaral <agamaralcanada@gmail.com>
2022-02-05 17:50:33 -0300

    first commit

---
+++ src/threadStudies/MyThread.java
@@ -0,0 +1,15 @@
+package threadStudies;
+
+public class MyThread extends Thread {
+    private String name;
+
+    public MyThread(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        for (int i = 0; i < 6; i++) {
+
+        }
+    }
+}
---
+++ src/threadStudies/Test.java
@@ -0,0 +1,7 @@
+package threadStudies;
+
+public class Test {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread("thread");
+    }
+}
